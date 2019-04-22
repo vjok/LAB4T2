db55b26a8e9f07294f7ed4610692686dcd94377a
Ville <t7kavi01@students.oamk.fi>
2019-04-22 19:49:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/predator/lab4t2/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.predator.lab4t2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebSocketClient client;
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+    private String messageToSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv_messages);
+        editText = findViewById(R.id.et_message);
+        button = findViewById(R.id.btn_send);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageToSend = editText.getText().toString();
+                client.send(messageToSend);
+                editText.setText(null);
+            }
+        });
+        connectWebSocketClient();
+    }
+
+    private void connectWebSocketClient() {
+        URI uri = null;
+        try {
+            uri = new URI("ws://obscure-waters-98157.herokuapp.com");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        client = new WebSocketClient(uri) {
+
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+                Toast.makeText(MainActivity.this, "Connection opened!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMessage(final String message) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(textView.getText() + "\n" + message);
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+            }
+
+            @Override
+            public void onError(Exception ex) {
+            }
+        };
+        client.connect();
+    }
+}
